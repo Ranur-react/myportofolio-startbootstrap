d25c224b02a0bd19e9eece1e71f97ac5bd2f9e3d
Ranur-react <rahmatnur845@gmail.com>
2024-07-12 17:59:34 +0700

    truncate teks

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,30 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// function toggleText() {
+//     const content = document.getElementById('experience-content');
+//     const button = document.getElementById('read-more-btn');
+
+//     if (content.classList.contains('show-all')) {
+//         content.classList.remove('show-all');
+//         button.textContent = 'Read More';
+//     } else {
+//         content.classList.add('show-all');
+//         button.textContent = 'Read Less';
+//     }
+// }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.read-more-btn');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            const content = this.previousElementSibling;
+
+            if (content.classList.contains('text-truncate')) {
+                content.classList.remove('text-truncate');
+                this.textContent = 'Read Less';
+            } else {
+                content.classList.add('text-truncate');
+                this.textContent = 'Read More';
+            }
+        });
+    });
+});
